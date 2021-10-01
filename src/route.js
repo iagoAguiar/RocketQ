07040291fa2bd694ea506588c749cc11fa6409bc
iagoAguiar <iagoaguiar202@gmail.com>
2021-10-01 16:49:28 -0300

    Adicionado o Banco de dados

--- src/route.js
+++ src/route.js
@@ -1,12 +1,17 @@
 const express = require('express');
+const questionControler = require("./controllers/QuestionController")
+const roomController = require("./controllers/RoomController")
 
 const route = express.Router();
 
-route.get('/', (req, res) =>{  res.render("index")}) 
+route.get('/', (req, res) =>{  res.render("index", {page: 'enter-room'})}) 
+route.get('/create-pass', (req, res) =>{  res.render("index", {page: 'create-pass'})}) 
 
-route.get('/room', (req, res) =>{  res.render("room")}) 
+route.get('/room/:room', (req, res) =>{  res.render("room")}) 
 
-route.get('/create-pass', (req, res) =>{  res.render("create-pass")}) 
 
+route.post('/question/:room/:question/:action', questionControler.index)
+
+route.post('/create-room', roomController.create);
 
 module.exports = route
