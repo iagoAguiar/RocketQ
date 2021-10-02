563aad7f6750c597a4c1d3b7a1f4d275746a95a2
iagoAguiar <iagoaguiar202@gmail.com>
2021-10-02 14:26:51 -0300

    finalizado projeto Discover - fundamentos

--- src/controllers/RoomController.js
+++ src/controllers/RoomController.js
@@ -6,21 +6,39 @@ module.exports = {
         const pass = req.body.password
 
         let roomId = "";
+        let isRoom = true;
+        
+         
 
-        for(let i =0 ;i< 6 ; i++){
-            roomId +=   Math.floor(Math.random() * 10).toString();
-
+        while(isRoom){
+            //  Gera o número da sala
+            for(let i =0 ;i< 6 ; i++){
+                roomId +=   Math.floor(Math.random() * 10).toString();
+    
+            }
+    
+            // Verificar se o número da sala existe
+    
+            const roomsExistsIds = await db.all(`SELECT id FROM rooms`);
+    
+           
+            isRoom = roomsExistsIds.some(roomId => {
+                roomId === roomId
+            })
         }
 
-        await db.run(`
-            INSERT INTO rooms(
-                id,
-                pass
-            ) VALUES(
-                ${parseInt(roomId)},
-                ${pass}
-            )
-        `);
+        if(!isRoom){
+            // Insere a sala no banco
+            await db.run(`
+                INSERT INTO rooms(
+                    id,
+                    pass
+                ) VALUES(
+                    ${parseInt(roomId)},
+                    ${pass}
+                )`);
+        }
+
 
    
 
@@ -28,7 +46,27 @@ module.exports = {
         await db.close();
 
         res.redirect(`/room/${roomId}`)
-    }
+    },
 
+    async open(req, res){
+        const db = await Database();
+
+        const roomId = req.params.room
+        const questions = await db.all(`SELECT * FROM questions WHERE room = ${roomId} and read = 0`);
+        const questionRead = await db.all(`SELECT * FROM questions WHERE room = ${roomId} and read = 1`);
+
+        let isNoQuestions;
+
+        if(questions.length ==0 && questionRead == 0){            
+                isNoQuestions = true;            
+        }
+
+        res.render("room", {roomId: roomId, questions: questions, questionsRead: questionRead, isNoQuestions: isNoQuestions})
+    },
+    enter(req, res){
+        const roomId = req.body.roomId
+
+        res.redirect(`/room/${roomId}`)
+    }
     
 }
--- src/route.js
+++ src/route.js
@@ -1,17 +1,20 @@
 const express = require('express');
-const questionControler = require("./controllers/QuestionController")
+const QuestionController = require('./controllers/QuestionController');
+const questionControler = require("./controllers/QuestionController");
+const RoomController = require('./controllers/RoomController');
 const roomController = require("./controllers/RoomController")
 
 const route = express.Router();
 
 route.get('/', (req, res) =>{  res.render("index", {page: 'enter-room'})}) 
+
 route.get('/create-pass', (req, res) =>{  res.render("index", {page: 'create-pass'})}) 
 
-route.get('/room/:room', (req, res) =>{  res.render("room")}) 
-
-
-route.post('/question/:room/:question/:action', questionControler.index)
-
+route.get('/room/:room', roomController.open) 
 route.post('/create-room', roomController.create);
+route.post('/enterroom', RoomController.enter);
+
+route.post('/question/create/:room', QuestionController.create)
+route.post('/question/:room/:question/:action', questionControler.index)
 
 module.exports = route
